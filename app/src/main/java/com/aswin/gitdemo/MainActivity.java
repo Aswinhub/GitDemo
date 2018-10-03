f4cd2be4b5334921698cd9ca63c177a8715e46b5
Aswin <aswinkumar95@gmail.com>
2018-10-03 10:50:33 +0530

    b1 merged to master

--- app/src/main/java/com/aswin/gitdemo/MainActivity.java
+++ app/src/main/java/com/aswin/gitdemo/MainActivity.java
@@ -11,4 +11,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     //new 1
+    //b2 line 1 added
+    //b1 line 1 added
+    //b2 cherry 1
+    //b2 cherry 2
+    //b2 cherry 3
+    //stash 1
+    //master updated
+    //master updated 2
+    //b2 added
+    //b1 added
+    //new b1
+    //new b1
 }
