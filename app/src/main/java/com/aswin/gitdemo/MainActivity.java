a76347226f0e1c0a95dc0821ecc137b246f845d9
Aswin <aswinkumar95@gmail.com>
2018-10-03 09:13:32 +0530

    comments removed

--- app/src/main/java/com/aswin/gitdemo/MainActivity.java
+++ app/src/main/java/com/aswin/gitdemo/MainActivity.java
@@ -10,14 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    //b2 line 1 added
-    //b1 line 1 added
-    //b2 cherry 1
-    //b2 cherry 2
-    //b2 cherry 3
-    //stash 1
-    //master updated
-    //master updated 2
-    //b2 added
-    //b1 added
 }
